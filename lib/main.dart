e782684ba2b21b20662bc743c0ab12a646574284
Anant Dubey <anantkumar312@gmail.com>
2024-01-11 15:00:02 +0530

    feat: :lipstick: add basic boiler

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/expenses.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Expenses(),
+      ),
+    ),
+  );
+}
