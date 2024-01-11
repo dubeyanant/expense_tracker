a12e6e65988a7c7723a752497df6000fc70ccf40
Anant Dubey <anantkumar312@gmail.com>
2024-01-11 15:05:14 +0530

    feat: :monocle_face: add expense data model class

---
+++ lib/models/expense.dart
@@ -0,0 +1,8 @@
+class Expense {
+  Expense({required this.amount, required this.title, required this.date});
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+}
