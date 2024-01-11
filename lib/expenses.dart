4d9b8c2601928cc0eb7dad9ebc3c6ef3a621b01a
Anant Dubey <anantkumar312@gmail.com>
2024-01-11 15:35:27 +0530

    feat: :sparkles: create basic expense list

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/expenses_list.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -10,13 +13,28 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpense = [
+    Expense(
+      title: "Flutter Course",
+      amount: 359.00,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "Cinema",
+      amount: 480.00,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text("The chart..."),
-          Text("Expenses list..."),
+          const Text("The chart..."),
+          Expanded(child: ExpensesList(expenses: _registeredExpense)),
         ],
       ),
     );
