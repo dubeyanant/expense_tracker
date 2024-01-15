6311933782ec0df488edc3789a6f8d02375fee55
Anant Dubey <anantkumar312@gmail.com>
2024-01-15 12:12:03 +0530

    feat: :sparkles: add chart

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,7 +1,8 @@
-import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
@@ -93,7 +94,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text("The chart..."),
+          Chart(expenses: _registeredExpense),
           Expanded(child: mainContent),
         ],
       ),
